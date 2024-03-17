a63162a1fa242acc75d82a987a73810c5425614d
AK Rahul <rahulanilkumar2023@gmail.com>
2024-03-17 11:09:37 +0530

    Creating Navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Navbar.css'
 import Logo from "../../Assets/logo2.png"
+import HomeLogo from "../../Assets/home-icon.svg"
+import AboutLogo from "../../Assets/about-logo.png"
 
 function Navbar() {
   return (
@@ -8,9 +10,9 @@ function Navbar() {
       <nav className="NavWrapper">
         <a className = "LogoLink" href = "/"><img className = "PageLogo" src = {Logo} alt= "BigBook"/></a>
         <div className="NavMenu">
-          <a href= "/home"><img src = {Logo}></img><span>HOME</span></a>
-          <a href= "/home"><img src = "src\Assets\home-icon.svg"></img><span>ABOUT US</span></a>
-          <a href= "/home"><img src = "src\Assets\home-icon.svg"></img><span>LOGIN</span></a>
+          <a href= "/home"><img src = {HomeLogo}></img><span>HOME</span></a>
+          <a href= "/home"><img src = {AboutLogo}></img><span>ABOUT US</span></a>
+          <a href= "/home"><img src = {HomeLogo}></img><span>LOGIN</span></a>
         </div>
       </nav>
     </div>
